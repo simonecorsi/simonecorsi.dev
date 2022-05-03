85b613acd65a11add32932f8be65c5b3cd9443f3
Simone Corsi <simonecorsi.rm@gmail.com>
2022-05-03 15:44:39 +0200

    chore: cleanup deps

--- prepare.js
+++ prepare.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
 let isCi = false;
 
 try {
-  isCi = require("is-ci");
+  isCi = require('is-ci');
 } catch (_) {
   isCi = true;
 }
 
 if (!isCi) {
-  require("husky").install();
+  require('husky').install();
 }
