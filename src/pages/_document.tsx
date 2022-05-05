16db3458128758ce8d96ac10d3a53c6e2e6633ce
Simone Corsi <simonecorsi.rm@gmail.com>
2022-05-05 09:46:45 +0200

    perf: use system fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,16 +5,17 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Karla:wght@300;500&display=swap"
             rel="stylesheet"
-          />
+          /> 
           <style
             dangerouslySetInnerHTML={{
               __html: `html,body{ font-family: 'Karla', sans-serif;}`,
             }}
           ></style>
+          */}
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="apple-touch-icon" href="/icon.png" />
           <link
