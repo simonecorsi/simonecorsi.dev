71d742235b8069a32e1310d30f1c794693e9d521
Simone Corsi <simonecorsi.dev@gmail.com>
2025-02-04 13:22:01 +0100

    chore: higher delay

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer({
-  output: 'export',
+  staticPageGenerationTimeout: 60 * 5, // Increase timeout to 3 minutes
+  output: "export",
   trailingSlash: true,
   eslint: {
     // Warning: Dangerously allow production builds to successfully complete even if
@@ -11,16 +12,16 @@ module.exports = withBundleAnalyzer({
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['raw.githubusercontent.com', 'avatars.githubusercontent.com'],
+    domains: ["raw.githubusercontent.com", "avatars.githubusercontent.com"],
   },
   webpack: (config) => {
     config.module.rules.push(
       ...[
         {
           test: /\.svg$/,
-          use: '@svgr/webpack',
+          use: "@svgr/webpack",
         },
-      ]
+      ],
     );
 
     return config;
