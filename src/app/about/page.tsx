f07d99d2cdb2a7c9254d149cf01551adeb2432f2
Simone Corsi <simonecorsi.dev@gmail.com>
2026-03-10 12:08:56 +0100

    refactor: about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,10 +11,11 @@ async function getData() {
 
 export default async function AboutPage() {
   const { data } = await getData();
+
   return (
     <div className="min-h-screen py-20 px-8 flex justify-center">
       <div
-        className="max-w-3xl w-full prose prose-lg lg:prose-xl prose-slate dark:prose-invert"
+        className="max-w-3xl w-full prose prose-lg lg:prose-xl prose-slate dark:prose-invert prose-about"
         // biome-ignore lint/security/noDangerouslySetInnerHtml: Needed to render bio markdown
         dangerouslySetInnerHTML={{ __html: data }}
       />
